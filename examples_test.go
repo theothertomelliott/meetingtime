c453ffd121e87ddd0ae43729b2b4b16180fbc15e
Tom Elliott <tom.w.elliott@gmail.com>
2016-09-11 23:49:35 -0400

    Move error examples to Schedule examples.

--- examples_test.go
+++ examples_test.go
@@ -25,8 +25,25 @@ func ExampleSchedule() {
 	}
 	fmt.Println(first.Format(time.UnixDate))
 
+	// Create a Schedule for a daily meeting at 2.30pm
+	schedule = NewDailySchedule(time.Date(2016, time.February, 1, 14, 30, 0, 0, time.UTC), 1)
+
+	// An ErrNoEarlierMeetings error is returned if you try to get the meeting before the first
+	_, err = schedule.Previous(time.Date(2016, time.February, 1, 14, 30, 0, 0, time.UTC))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// An ErrNoEarlierMeetings error is also returned for calling previous on any earlier date
+	_, err = schedule.Previous(time.Date(2016, time.January, 31, 0, 0, 0, 0, time.UTC))
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	// Output: Thu Mar 10 18:00:00 UTC 2016
 	// Sun Jan 10 18:00:00 UTC 2016
+	// no meetings on or before this date
+	// no meetings on or before this date
 }
 
 func ExampleScheduleSlice() {
@@ -53,24 +70,4 @@ func ExampleScheduleSlice() {
 	fmt.Println(secondInOct.Format(time.UnixDate))
 	// Output: Mon Oct  3 19:00:00 UTC 2016
 	// Mon Oct 17 19:00:00 UTC 2016
-}
-
-func ExampleErrNoEarlierMeetings() {
-	// Create a Schedule for a daily meeting at 2.30pm
-	schedule := NewDailySchedule(time.Date(2016, time.February, 1, 14, 30, 0, 0, time.UTC), 1)
-
-	// An ErrNoEarlierMeetings error is returned if you try to get the meeting before the first
-	_, err := schedule.Previous(time.Date(2016, time.February, 1, 14, 30, 0, 0, time.UTC))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// An ErrNoEarlierMeetings error is also returned for calling previous on any earlier date
-	_, err = schedule.Previous(time.Date(2016, time.January, 31, 0, 0, 0, 0, time.UTC))
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	// Output: no meetings on or before this date
-	// no meetings on or before this date
 }
